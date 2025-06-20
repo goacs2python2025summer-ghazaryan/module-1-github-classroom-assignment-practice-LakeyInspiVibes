1e38340e341a914f327564f72c2f5b0adce8bed6
Krish <krish@Mac-1128.lan>
2025-06-20 12:22:08 -0500

    Wrote Hello World Program

---
+++ helloworld.py
@@ -0,0 +1,4 @@
+print("Hello world!")
+print("I went skiing and I didn't break anything, I think")
+#Someting interesting I did over the break:
+print("Over break, I travelled to Bali, Indonesia and I tried new foods, tanned on beaches, and participated in water sports!")
